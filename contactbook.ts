b874e5be38c4eaed7dd777375c431b88413e5c4d
Sonal Maheshwari <sonal.obedient@gmail.com>
2026-01-22 19:44:39 +0100

    created diff functions for c.book

--- contactbook.ts
+++ contactbook.ts
@@ -7,8 +7,33 @@ interface Contact {
 }
 let contacts: Contact[] = [];
 
-// function 1.
+// function 1. Add Contacts
 const addContact=(contact: Contact) => {
 contacts.push(contact);
 console.log("contact added:", contact.name)
 }
+
+//function 2. List Contacts-
+const listContacts=() =>{
+  for (let contact of contacts) {
+    console.log( " Here is contact list",
+      contact.id,
+      contact.name,
+      contact.email,
+      contact.phone
+    );
+  }
+}
+
+
+// Function 3 findByName-
+ function findByName(name: string): Contact[] {
+  let result = contacts.filter(c => c.name === name);
+  console.log("Found", result.length, "contact(s)");
+  return result;
+}
+ 
+// function 4. removeById
+
+
+
